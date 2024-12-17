6b68f8e3c94d23e0f9ad98bca5e808b797c8ba2d
Ali Divband <alidivbnd@gmail.com>
2024-12-17 20:49:27 +0100

    import settings into main program file

--- game.py
+++ game.py
@@ -7,9 +7,10 @@ class StellarStriker:
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display,set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Stellar Striker")
         self.bg_color = (230, 230, 230)
+        self.settings = Settings()
 
     def run_game(self):
         while True:
@@ -19,8 +20,7 @@ class StellarStriker:
 
             pygame.display.flip()
             self.clock.tick(60)
-
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
 if __name__ == '__main__':
     ss = StellarStriker()
