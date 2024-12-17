0cdfbaeb8abc8fb931ae1745c7035e1e16c77f65
Ali Divband <alidivbnd@gmail.com>
2024-12-17 20:40:35 +0100

    writing background color and add frame rate with clock

--- game.py
+++ game.py
@@ -6,20 +6,21 @@ class StellarStriker:
 
     def __init__(self):
         pygame.init()
-
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display,set_mode((1200, 800))
         pygame.display.set_caption("Stellar Striker")
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
-
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT():
                     sys.exit()
 
-
-
             pygame.display.flip()
+            self.clock.tick(60)
+
+            self.screen.fill(self.bg_color)
 
 if __name__ == '__main__':
     ss = StellarStriker()
